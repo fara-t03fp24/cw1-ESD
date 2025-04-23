3fe932f834daf7e2666e5fb47748de75df5ee781
fara-t03fp24 <t03fp24@abdn.ac.uk>
2025-04-23 19:12:56 +0500

    Added initial weather data loader command

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+#Load initial weather data
+python manage.py load_weather_data --records-per-city 300
