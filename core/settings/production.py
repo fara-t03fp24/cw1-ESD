6c123b67ff898e9f8d9a215798ab7e4450a4f7db
fara-t03fp24 <t03fp24@abdn.ac.uk>
2025-04-23 17:43:53 +0500

    Add specific Render.com domain to allowed hosts and CSRF trusted origins

--- core/settings/production.py
+++ core/settings/production.py
@@ -10,7 +10,7 @@ DEBUG = False
 # Use environment variable for SECRET_KEY, falling back to base.py's value
 SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cw1-esd.onrender.com', '.onrender.com']
 
 ###################################################################
 # Django security
@@ -21,13 +21,15 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
-    os.environ.get('RENDER_EXTERNAL_URL', 'https://your-app-name.onrender.com')
+    'https://cw1-esd.onrender.com',
+    'https://*.onrender.com'
 ]
 
 ###################################################################
 # Static files
 ###################################################################
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 ###################################################################
 # Database
